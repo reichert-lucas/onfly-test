887069767786463ca63e582feec6d43817508f5e
Lucas Coelho Reichert <lucas3d4783@gmail.com>
2025-06-28 23:28:14 -0300

    feat: cp env example to env

--- front/start.sh
+++ front/start.sh
@@ -6,4 +6,11 @@ if ! [ -d "node_modules" ]; then
     npm install
 fi
 
+if [ -e ".env" ]; then
+    echo ".env file already exists"
+else
+    echo "Creating .env file"
+    cp .env.example .env
+fi
+
 npm run dev
